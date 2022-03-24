882739260ce9137b7b1c231ac1f1b699db374854
David Holas <DavidHolas95@gmail.com>
2022-03-24 18:22:45 +0100

    init commit

---
+++ julie/src/main/java/com/davidholas/julie/JulieApplication.java
@@ -0,0 +1,13 @@
+package com.davidholas.julie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulieApplication.class, args);
+	}
+
+}
---
+++ julie/src/test/java/com/davidholas/julie/JulieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidholas.julie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
