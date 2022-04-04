4c0aae926ed4355cdffda43807089945a9d98f23
David Holas <DavidHolas95@gmail.com>
2022-04-04 11:30:22 +0200

    allow cors for events

--- julie/src/main/java/com/davidholas/julie/JulieApplication.java
+++ julie/src/main/java/com/davidholas/julie/JulieApplication.java
@@ -3,7 +3,10 @@ package com.davidholas.julie;
 import com.davidholas.julie.config.JulieMapperConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Import({
 		JulieMapperConfig.class
@@ -13,6 +16,16 @@ public class JulieApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JulieApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/events").allowedOrigins("http://localhost:8090");
+			}
+		};
 	}
 
 }
